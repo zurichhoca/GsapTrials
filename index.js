903ac8b75676cfab6650984c0e20c13262241167
Zürih <104237291+zurichhoca@users.noreply.github.com>
2025-09-06 13:26:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+gsap.registerPlugin(TextPlugin, SplitText, ScrollTrigger); 
+
+let text = document.querySelector("#text")
+let point = document.querySelector(".point")
+let hero = document.querySelector(".hero")
+let heroSpan = document.querySelector(".hero-span")
+let heroH1 = document.querySelector(".hero-h1")
+let heroBtn = document.querySelector(".hero-btn")
+let split = SplitText.create(text,{ type: "chars,words,lines" });
+
+
+
+heroBtn.addEventListener ("click",()=>{
+    gsap.to(heroBtn,{
+        text: "Reserved",
+        backgroundColor: "green",
+        duration: 1,
+        ease: "elastic.out(1,0.3)"
+    }),
+    gsap.to(heroSpan,{
+        text: "See you soon!",
+        color: "green",   
+        duration: 1,
+        ease: "elastic.out(1,0.3)"
+    })                                
+
+
+// let splitHero = SplitText.create([heroSpan,heroH1], {type: "words,chars"})
+
+// gsap.from(splitHero.words, {
+//    x: 100,
+//   duration:0.9,
+//   opacity: 0.2,
+//   rotation: "random(-80, 80)",
+//   ease: "back-out(0.5,7,none)",
+//   stagger: 0.1
+// });
+
+// gsap.from(split.words, {
+//   y: -200,
+//   scale:100,
+//   textShadow: "0px 0px 20px #ff0000",
+//   duration: 0.9,
+//   opacity: 0.3,
+//   rotation: "random(-80, 80)",
+//   duration: 1.2,
+//   ease: "expoScale(0.5,7,none)",
+//   stagger: 0.1
+// });
+
+
+
+
+
+
+
+
+
+
+
+
