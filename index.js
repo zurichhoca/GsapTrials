cada9d5addc3f06136bab058c271ce25fb0a48a5
Zurich Attabach <zurichattabach@192.168.1.12>
2025-09-06 19:29:40 +0300

    adding font and few animations

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+
 gsap.registerPlugin(TextPlugin, SplitText, ScrollTrigger); 
 
-let container = document.querySelector(".container")
 let text = document.querySelector("#text")
 let point = document.querySelector(".point")
 let hero = document.querySelector(".hero")
@@ -11,31 +11,42 @@ let split = SplitText.create(text,{ type: "chars,words,lines" });
 
 
 
+heroBtn.addEventListener("click",function(){
+            gsap.to(heroBtn, {
+            text: "Reserved",
+            backgroundColor: "#111",
+            color: "white",
+            duration: 1.4,
+            ease: "power2.inOut",
+
+
+        })
+})
                               
 
 
-let splitHero = SplitText.create([heroSpan,heroH1], {type: "words,chars"})
+// let splitHero = SplitText.create([heroSpan,heroH1], {type: "words,chars"})
 
-gsap.from(splitHero.words, {
-   x: 100,
-  duration:0.9,
-  opacity: 0.2,
-  rotation: "random(-80, 80)",
-  ease: "back-out(0.5,7,none)",
-  stagger: 0.1
-});
+// gsap.from(splitHero.words, {
+//    x: 100,
+//   duration:0.9,
+//   opacity: 0.2,
+//   rotation: "random(-80, 80)",
+//   ease: "back-out(0.5,7,none)",
+//   stagger: 0.1
+// });
 
-gsap.from(split.words, {
-  y: -200,
-  scale:100,
-  textShadow: "0px 0px 20px #ff0000",
-  duration: 0.9,
-  opacity: 0.3,
-  rotation: "random(-80, 80)",
-  duration: 1.2,
-  ease: "expoScale(0.5,7,none)",
-  stagger: 0.1
-});
+// gsap.from(split.words, {
+//   y: -200,
+//   scale:100,
+//   textShadow: "0px 0px 20px #ff0000",
+//   duration: 0.9,
+//   opacity: 0.3,
+//   rotation: "random(-80, 80)",
+//   duration: 1.2,
+//   ease: "expoScale(0.5,7,none)",
+//   stagger: 0.1
+// });
 
 
 
